c06571cc3ef13c015be33bd50f24b9f417269068
camila-marquess <camilamarquesoliveira@outlook.com>
2023-02-09 21:46:06 -0300

    refactor: organizing functions to another file

--- src/main/scala/dataAnalysis/netflixAnalysis.scala
+++ src/main/scala/dataAnalysis/netflixAnalysis.scala
@@ -1,8 +1,6 @@
 package dataAnalysis
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.to_date
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{SparkSession}
 import org.apache.spark.sql.functions.col
 import util.mainFunctions._
 
@@ -35,14 +33,6 @@ object netflixAnalysis extends App {
     .drop(col("date_added"))
 
 
-//EDA
-
-  //Quantos filmes/séries foram lançados por ano?
-  //Qual dia que mais teve filmes/séries adicionadas na netflix?
-  //Quantidade de filmes/séries adicionados por dia
-  //Qual país teve mais filmes/séries produzidos pela netflix?
-  //Quantidade de filmes/séries por duração
-
   val show_type = Seq("Movie", "TV Show")
 
   for (show_types <- show_type) {
@@ -69,99 +59,5 @@ object netflixAnalysis extends App {
     data
       .transform(topGenresPerShow(show_types))
       .show(20)
-  }
-
-  //
-
-
-
-
-  def concatDateColumns(new_date_column: String, year_column: String, month_column: String, day_column: String)(df: DataFrame):DataFrame = {
-    df
-      .withColumn(new_date_column,
-        concat_ws("-", col(year_column), col(month_column), col(day_column)))
-  }
-
-  def transformStringDate(column_selected: String, column_transformed: String, new_column_type: String)(df: DataFrame): DataFrame = {
-    df
-      .withColumn(column_transformed, to_date(col(column_selected)))
-      .withColumn(new_column_type, col(new_column_type).cast("int"))
-  }
-
-  def getMainGenre(column_selected: String, new_column: String)(df: DataFrame): DataFrame = {
-    val extractGenre = udf((genre: String) => genre.split(",")(0))
-    df
-      .withColumn(new_column, extractGenre(col(column_selected)))
-  }
-
-  def transformCountry(column_selected: String)(df: DataFrame): DataFrame = {
-    val nonCountries = Seq("Aziz Ansari", "Chuck D.", "Dominic Costa", "Doug Plaut", "Francesc Orella",
-      "Justin \"\"Alyssa Edwards\"\" Johnson", "Lachion Buckingham", "Leonardo Sbaraglia", "Michael Cavalieri",
-      "Rob Morgan", "Tantoo Cardinal", "Tobechukwu \"\"iLLbliss\"\" Ejiofor")
-
-    df
-      .withColumn(column_selected,
-        when(trim(col(column_selected)).isin(nonCountries:_*), null)
-      .otherwise(trim(col(column_selected))))
-  }
-
-  def transformDurationColumn(column_selected: String)(df: DataFrame): DataFrame = {
-    val nonDuration = Seq("Alan Cumming", "Benn Northover", "Donnell Rawlings", "Itziar Aizpuru", "Jimmy Herman\"",
-      "MC Eiht", "Maurice Everett\"", "Sharon Ooja\"", "Wanda Sykes", "United States", "1994")
-
-    df
-      .withColumn(column_selected,
-        when(trim(col(column_selected)).isin(nonDuration:_*), null)
-          .otherwise(trim(col(column_selected))))
-  }
-
-  def showsPerPeriod(show_type: String)(df: DataFrame): DataFrame = {
-    df
-      .filter(col("type").equalTo(show_type))
-      .groupBy(col("effective_date"))
-      .agg(countDistinct(col("show_id")).as(s"Count ${show_type}"))
-      .orderBy(col("effective_date").desc)
-  }
-
-  def showsReleasedPerYear(show_type: String)(df: DataFrame): DataFrame = {
-    df
-      .filter(col("type").equalTo(show_type))
-      .groupBy(col("release_year"))
-      .agg(countDistinct(col("show_id")).as(s"Total Quantity ${show_type}"))
-      .orderBy(col(s"Total Quantity ${show_type}").desc)
-  }
-
-  def maxShowPerDay(show_type: String)(df: DataFrame): DataFrame = {
-    df
-      .filter(col("type").equalTo(show_type))
-      .groupBy(col("effective_date"))
-      .agg(count(col("show_id")).as(s"Max ${show_type} Added in a Day"))
-      .orderBy(col(s"Max ${show_type} Added in a Day").desc)
-      .limit(1)
-  }
-
-  def showsPerCountry(show_type: String)(df: DataFrame): DataFrame = {
-    df
-      .filter(col("type").equalTo(show_type))
-      .groupBy(col("country"))
-      .agg(countDistinct(col("show_id")).as(s"Count ${show_type}"))
-      .orderBy(col(s"Count ${show_type}").desc)
-  }
-
-  def showsPerDuration(show_type: String)(df: DataFrame): DataFrame = {
-    df
-      .filter(col("type").equalTo(show_type))
-      .groupBy(col("duration"))
-      .agg(countDistinct(col("show_id")).as(s"Count ${show_type}"))
-      .orderBy(col(s"Count ${show_type}").desc)
-  }
-
-  def topGenresPerShow(show_type: String)(df: DataFrame): DataFrame = {
-    df
-      .filter(col("type").equalTo(show_type))
-      .groupBy(col("genre"))
-      .agg(count(col("show_id")).as(s"Top 10 ${show_type} per Genre"))
-      .orderBy(col(s"Top 10 ${show_type} per Genre").desc)
-      .limit(10)
   }
 }
--- src/main/scala/util/mainFunctions.scala
+++ src/main/scala/util/mainFunctions.scala
@@ -2,7 +2,7 @@ package util
 
 import dataAnalysis.netflixAnalysis.spark
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, desc, month, size, split, to_date, udf, when}
+import org.apache.spark.sql.functions.{col, concat_ws, count, countDistinct, desc, month, size, split, to_date, trim, udf, when}
 
 object mainFunctions {
 
@@ -86,5 +86,166 @@ object mainFunctions {
     val extractYear = udf((date: String) => date.split(" ")(2))
     df
       .withColumn(new_column, extractYear(col(column_selected)))
+  }
+
+  /**
+   * Method used to concatenate date columns
+   * @param new_date_column
+   * @param year_column
+   * @param month_column
+   * @param day_column
+   * @param df
+   * @return
+   */
+  def concatDateColumns(new_date_column: String, year_column: String, month_column: String, day_column: String)(df: DataFrame):DataFrame = {
+    df
+      .withColumn(new_date_column,
+        concat_ws("-", col(year_column), col(month_column), col(day_column)))
+  }
+
+  /**
+   * Method used to transform string to date type
+   * @param column_selected
+   * @param column_transformed
+   * @param new_column_type
+   * @param df
+   * @return
+   */
+  def transformStringDate(column_selected: String, column_transformed: String, new_column_type: String)(df: DataFrame): DataFrame = {
+    df
+      .withColumn(column_transformed, to_date(col(column_selected)))
+      .withColumn(new_column_type, col(new_column_type).cast("int"))
+  }
+
+  /**
+   * Method used to get the main movie/tv show genre
+   * @param column_selected
+   * @param new_column
+   * @param df
+   * @return
+   */
+  def getMainGenre(column_selected: String, new_column: String)(df: DataFrame): DataFrame = {
+    val extractGenre = udf((genre: String) => genre.split(",")(0))
+    df
+      .withColumn(new_column, extractGenre(col(column_selected)))
+  }
+
+  /**
+   * Method used to clean country column
+   * @param column_selected
+   * @param df
+   * @return
+   */
+  def transformCountry(column_selected: String)(df: DataFrame): DataFrame = {
+    val nonCountries = Seq("Aziz Ansari", "Chuck D.", "Dominic Costa", "Doug Plaut", "Francesc Orella",
+      "Justin \"\"Alyssa Edwards\"\" Johnson", "Lachion Buckingham", "Leonardo Sbaraglia", "Michael Cavalieri",
+      "Rob Morgan", "Tantoo Cardinal", "Tobechukwu \"\"iLLbliss\"\" Ejiofor")
+
+    df
+      .withColumn(column_selected,
+        when(trim(col(column_selected)).isin(nonCountries:_*), null)
+          .otherwise(trim(col(column_selected))))
+  }
+
+  /**
+   * Method used to clean duration column
+   * @param column_selected
+   * @param df
+   * @return
+   */
+  def transformDurationColumn(column_selected: String)(df: DataFrame): DataFrame = {
+    val nonDuration = Seq("Alan Cumming", "Benn Northover", "Donnell Rawlings", "Itziar Aizpuru", "Jimmy Herman\"",
+      "MC Eiht", "Maurice Everett\"", "Sharon Ooja\"", "Wanda Sykes", "United States", "1994")
+
+    df
+      .withColumn(column_selected,
+        when(trim(col(column_selected)).isin(nonDuration:_*), null)
+          .otherwise(trim(col(column_selected))))
+  }
+
+  /**
+   * Method used to count distinct movies/tv shows per day
+   * @param show_type
+   * @param df
+   * @return
+   */
+  def showsPerPeriod(show_type: String)(df: DataFrame): DataFrame = {
+    df
+      .filter(col("type").equalTo(show_type))
+      .groupBy(col("effective_date"))
+      .agg(countDistinct(col("show_id")).as(s"Count ${show_type}"))
+      .orderBy(col("effective_date").desc)
+  }
+
+  /**
+   * Method used to count distinct movies/tv shows per released year
+   * @param show_type
+   * @param df
+   * @return
+   */
+  def showsReleasedPerYear(show_type: String)(df: DataFrame): DataFrame = {
+    df
+      .filter(col("type").equalTo(show_type))
+      .groupBy(col("release_year"))
+      .agg(countDistinct(col("show_id")).as(s"Total Quantity ${show_type}"))
+      .orderBy(col(s"Total Quantity ${show_type}").desc)
+  }
+
+  /**
+   * Method used to get day which had more movies/tv shows added
+   * @param show_type
+   * @param df
+   * @return
+   */
+  def maxShowPerDay(show_type: String)(df: DataFrame): DataFrame = {
+    df
+      .filter(col("type").equalTo(show_type))
+      .groupBy(col("effective_date"))
+      .agg(count(col("show_id")).as(s"Max ${show_type} Added in a Day"))
+      .orderBy(col(s"Max ${show_type} Added in a Day").desc)
+      .limit(1)
+  }
+
+  /**
+   * Method used to count movies/tv shows per country
+   * @param show_type
+   * @param df
+   * @return
+   */
+  def showsPerCountry(show_type: String)(df: DataFrame): DataFrame = {
+    df
+      .filter(col("type").equalTo(show_type))
+      .groupBy(col("country"))
+      .agg(countDistinct(col("show_id")).as(s"Count ${show_type}"))
+      .orderBy(col(s"Count ${show_type}").desc)
+  }
+
+  /**
+   * Method used to count movies/tv shows per duration
+   * @param show_type
+   * @param df
+   * @return
+   */
+  def showsPerDuration(show_type: String)(df: DataFrame): DataFrame = {
+    df
+      .filter(col("type").equalTo(show_type))
+      .groupBy(col("duration"))
+      .agg(countDistinct(col("show_id")).as(s"Count ${show_type}"))
+      .orderBy(col(s"Count ${show_type}").desc)
+  }
+
+  /**
+   * Method used to get top 10 genres per movies/tv shows
+   * @param show_type
+   * @param df
+   * @return
+   */
+  def topGenresPerShow(show_type: String)(df: DataFrame): DataFrame = {
+    df
+      .filter(col("type").equalTo(show_type))
+      .groupBy(col("genre"))
+      .agg(count(col("show_id")).as(s"Top 10 ${show_type} per Genre"))
+      .orderBy(col(s"Top 10 ${show_type} per Genre").desc)
+      .limit(10)
   }
 }
